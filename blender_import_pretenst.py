ac2022e964cb1ea87adb911a0d5a33d4674095b0
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2020-02-12 17:56:14 +0100

    Add prototype scene and prototype.blend + other stuff

--- blender_import_pretenst.py
+++ blender_import_pretenst.py
@@ -1,22 +1,25 @@
-import bpy
-
 import csv
-from dataclasses import dataclass
-from os import path
-from datetime import datetime
-import mathutils
 import math
-import time
+from dataclasses import dataclass
+from datetime import datetime
+from os import path
+
+import bpy
+from mathutils import Vector
+from bpy.props import StringProperty, PointerProperty, FloatProperty
+from bpy.types import Operator
+from bpy_extras.io_utils import ImportHelper
 
 
 @dataclass
 class Joint:
     index: int
-    co: mathutils.Vector
+    co: Vector
 
 
 @dataclass
 class Interval:
+    index: int
     alpha: Joint
     omega: Joint
     type: str
@@ -25,19 +28,6 @@ class Interval:
     linear_density: float
     role: str
     length: float
-
-
-INTERVAL_ROLES = [
-    ("NexusPush", "NexusPush", "NexusPush", 0),
-    ("ColumnPush", "ColumnPush", "ColumnPush", 1),
-    ("Triangle", "Triangle", "Triangle", 2),
-    ("Ring", "Ring", "Ring", 3),
-    ("NexusCross", "NexusCross", "NexusCross", 4),
-    ("ColumnCross", "ColumnCross", "ColumnCross", 5),
-    ("BowMid", "BowMid", "BowMid", 6),
-    ("BowEnd", "BowEnd", "BowEnd", 7),
-    ("FacePull", "FacePull", "FacePull", 8),
-]
 
 
 def float_comma(s):
@@ -50,14 +40,15 @@ def joint_csv_to_dataclass(joint_csv):
     z = float_comma(joint_csv['z'])
     return Joint(
         index=int(joint_csv['index']),
-        co=mathutils.Vector((x, y, z)),
+        co=Vector((x, y, z)),
     )
 
 
-def interval_csv_to_dataclass(interval_csv, joints_dict):
+def interval_csv_to_dataclass(index, interval_csv, joints_dict):
     iv_joints = interval_csv['joints'].replace('=', '').replace('"', '')
     alpha_idx, omega_idx = iv_joints.split(',')
     return Interval(
+        index=index,
         alpha=joints_dict[int(alpha_idx)],
         omega=joints_dict[int(omega_idx)],
         type=interval_csv['type'],
@@ -69,70 +60,80 @@ def interval_csv_to_dataclass(interval_csv, joints_dict):
     )
 
 
-def load_csv(dirpath, filepath):
+def load_csv_rows(dirpath, filepath):
     p = path.join(dirpath, filepath)
     with open(p, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f, delimiter=";")
         return [row for row in reader]
 
 
-def load_intervals_from_csv(dirpath):
-    joints_csv = load_csv(dirpath, "joints.csv")
+def load_pretenst_from_csv(dirpath):
+    joints_csv = load_csv_rows(dirpath, "joints.csv")
     joints = [joint_csv_to_dataclass(joint_csv) for joint_csv in joints_csv]
     joints_dict = {
         joint.index: joint
         for joint in joints
     }
-    intervals_csv = load_csv(dirpath, "intervals.csv")
+    intervals_csv = load_csv_rows(dirpath, "intervals.csv")
     intervals = [
-        interval_csv_to_dataclass(interval_csv, joints_dict)
-        for interval_csv in intervals_csv
+        interval_csv_to_dataclass(i, interval_csv, joints_dict)
+        for i, interval_csv in enumerate(intervals_csv)
     ]
-    return intervals
+    return intervals, joints
 
 
-def test():
-    bpy.data.metaballs["Mball"].elements.new(type="PLANE")
+def track_axis_to_vector(track_axis: str) -> Vector:
+    if track_axis == "POS_X":
+        return Vector((1, 0, 0))
+    if track_axis == "POS_Y":
+        return Vector((0, 1, 0))
+    if track_axis == "POS_Z":
+        return Vector((0, 0, 1))
+    raise ValueError(f"Cannot convert tracking axis {track_axis}")
 
 
-def read_pretenst_csv(self, context, filepath):
-    dirpath = path.dirname(filepath)
-    _, pretenst_name = path.split(dirpath)
-    intervals = load_intervals_from_csv(dirpath)
-    self.report({'INFO'}, f"Pretenst: Loaded {len(intervals)} intervals from {dirpath}.")
-    collection_name = f"Pretenst: {pretenst_name} ({datetime.now()})"
-    collection = bpy.data.collections.new(name=collection_name)
-    context.scene.collection.children.link(collection)
-    for i, interval in enumerate(intervals):
-        obj_name = f"I{i} {interval.role} (J{interval.alpha.index} ~ J{interval.omega.index})"
-        mball = bpy.data.metaballs.new(name=obj_name)
-        obj = bpy.data.objects.new(mball.name, mball)
-        collection.objects.link(obj)
-        arrow = interval.omega.co - interval.alpha.co
-        rot = mathutils.Vector((1.0, 0.0, 0.0)).rotation_difference(arrow)
-        obj.location = interval.alpha.co.lerp(interval.omega.co, 0.5)
-        thiccness = math.sqrt(interval.elasticity) * 2
-        if interval.type == "Pull":
-            thiccness /= 4
-        obj.scale = (arrow.length / 2.3, thiccness, thiccness)
-        obj.rotation_mode = 'QUATERNION'
-        obj.rotation_quaternion = rot
-        mball.elements.new(type='BALL')
-        obj.pretenst_role = interval.role
-    return {'FINISHED'}
+def create_interval_node(intv: Interval, prototype_scene: bpy.types.Scene) -> bpy.types.Object:
+    alpha = intv.alpha
+    omega = intv.omega
+
+    intv_node = prototype_scene.objects[intv.type].copy()
+    intv_node.name = f"I{intv.index} {intv.role} (J{alpha.index} ~ J{omega.index})"
+
+    intv_arrow = omega.co - alpha.co
+    track_axis = track_axis_to_vector(intv_node.track_axis)
+
+    # Translation
+    intv_node.location = alpha.co.lerp(omega.co, 0.5)
+
+    # Rotation
+    rotation = track_axis.rotation_difference(intv_arrow)
+    intv_node.rotation_mode = 'QUATERNION'
+    intv_node.rotation_quaternion = rotation
+
+    # Scale
+    diameter_coeff = math.sqrt(intv.elasticity) * 200
+    diameter_delta = diameter_coeff * (Vector((1, 1, 1)) - track_axis)
+    length_delta = track_axis * intv_arrow.length
+    scale_delta = diameter_delta + length_delta
+    intv_node.scale.x *= scale_delta.x
+    intv_node.scale.y *= scale_delta.y
+    intv_node.scale.z *= scale_delta.z
+
+    return intv_node
 
 
-# ImportHelper is a helper class, defines filename and
-# invoke() function which calls the file selector.
-from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty
-from bpy.types import Operator
+def create_joint_node(jt: Joint, prototype_scene: bpy.types.Scene) -> bpy.types.Object:
+    joint_node = prototype_scene.objects["Joint"].copy()
+    joint_node.name = f"J{jt.index}"
+    joint_node.location = jt.co
+
+    return joint_node
 
 
 class ImportPretenst(Operator, ImportHelper):
     """Import a directory of Pretenst CSV files describing a tensegrity structure"""
-    bl_idname = "import_data.pretenst"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Import"
+    bl_idname = "pretenst.do_import"
+    bl_label = "Pretenst Import"
 
     # ImportHelper mixin class uses this
     filename_ext = ".csv"
@@ -144,7 +145,37 @@ class ImportPretenst(Operator, ImportHelper):
     )
 
     def execute(self, context):
-        return read_pretenst_csv(self, context, self.filepath)
+        do_import_pretenst_csv(self, context)
+        return {'FINISHED'}
+
+
+def clean_main_scene(scene: bpy.types.Scene):
+    for (name, collection) in scene.collection.children.items():
+        if not name.startswith("Pretenst"):
+            continue
+        print(f"Deleting collection '{name}'")
+        for obj in collection.objects.values():
+            bpy.data.objects.remove(obj, do_unlink=True)
+        bpy.data.collections.remove(collection, do_unlink=True)
+
+
+def do_import_pretenst_csv(self: ImportPretenst, context):
+    dirpath = path.dirname(self.filepath)
+    _, pretenst_name = path.split(dirpath)
+    intervals, joints = load_pretenst_from_csv(dirpath)
+    self.report({'INFO'}, f"Pretenst: Loaded {len(intervals)} intervals and {len(joints)} joints from {dirpath}.")
+    collection_name = f"Pretenst: {pretenst_name}"
+    collection = bpy.data.collections.new(name=collection_name)
+    main_scene = bpy.data.scenes["Scene"]
+    clean_main_scene(main_scene)
+    main_scene.collection.children.link(collection)
+    prototype_scene = bpy.data.scenes["Prototype"]
+    for interval in intervals:
+        intv_node = create_interval_node(interval, prototype_scene)
+        collection.objects.link(intv_node)
+    for joint in joints:
+        joint_node = create_joint_node(joint, prototype_scene)
+        collection.objects.link(joint_node)
 
 
 # Only needed if you want to add into a dynamic menu
@@ -156,18 +187,14 @@ def register():
     bpy.utils.register_class(ImportPretenst)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
-    bpy.types.Object.pretenst_role = bpy.props.EnumProperty(items=INTERVAL_ROLES)
-
 
 def unregister():
     bpy.utils.unregister_class(ImportPretenst)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
-
-    del bpy.types.Object.pretenst_role
 
 
 if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.import_data.pretenst('INVOKE_DEFAULT')
+    bpy.ops.pretenst.do_import('INVOKE_DEFAULT')
